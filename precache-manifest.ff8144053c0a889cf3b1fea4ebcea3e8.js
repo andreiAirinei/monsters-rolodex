6559d057aab8fa089c3655b8d3b217374f01f94f
Andrei Airinei <airinei.andrei@gmail.com>
2019-08-10 15:27:09 +0100

    Updates

---
+++ precache-manifest.ff8144053c0a889cf3b1fea4ebcea3e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25235ebb08add797e1df0981be0810ab",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "086d5d292f0b64d8a170",
+    "url": "/monsters-rolodex/static/css/main.904d50d1.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "086d5d292f0b64d8a170",
+    "url": "/monsters-rolodex/static/js/main.6ca1ce25.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
